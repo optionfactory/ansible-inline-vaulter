bf51f8a5e9629533e5c3cc66ed457fddc91352f6
Enrico Falanga <enrico.falanga@optionfactory.net>
2023-11-24 11:16:59 +0100

    improved error handling

--- src/ansible.rs
+++ src/ansible.rs
@@ -1,11 +1,11 @@
 use std::process::{Command, Stdio};
 
 pub struct Ansible {
-    pub vault: String
+    pub vault: String,
 }
 
 impl Ansible {
-    pub fn decrypt(&self, s: &String) -> Result<String, String>{
+    pub fn decrypt(&self, s: &String) -> Result<String, String> {
         let arg_str = format!("--vault-password-file={}", self.vault);
         let args = vec!["decrypt", &arg_str];
         let mut ansible_vault = Command::new("ansible-vault")
@@ -24,9 +24,9 @@ impl Ansible {
         let output = ansible_vault.wait_with_output()
             .expect("Internal error, failed to wait on child");
         match output.status.code() {
-            Some(0) => Ok(format!("{}",String::from_utf8_lossy(&output.stdout))),
-            Some(code) => Err(format!("Error {} decrypting", code)),
-            None => Ok(String::from(""))
+            Some(0) => Ok(format!("{}", String::from_utf8_lossy(&output.stdout))),
+            Some(code) => Err(format!("Error {code} decrypting: {}", String::from_utf8_lossy(&output.stdout))),
+            None => Ok(String::from("No exit code"))
         }
     }
 }
