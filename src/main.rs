f8679420a88efc04c62080a90bcfd5b80af0520e
Enrico Falanga <enrico.falanga@optionfactory.net>
2023-11-28 10:36:50 +0100

    :sparkles: get vault file location from ansible.cfg

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::fs;
+use std::{env, fs};
 
 use clap::Parser;
+use log::warn;
+use regex::Regex;
 
 use crate::ansible::Ansible;
 
@@ -13,17 +15,37 @@ struct Args {
     #[arg(short, long)]
     secrets_file: String,
     #[arg(short, long)]
-    vault_password_file: String,
+    vault_password_file: Option<String>,
 }
 
 fn main() {
     let args = Args::parse();
-    let file = args.secrets_file;
-    let content = fs::read_to_string(file).expect("Should have been able to read the file");
+    let secrets_file = args.secrets_file;
+    let content = fs::read_to_string(secrets_file).expect("Should have been able to read the file");
+    let vault_file = args.vault_password_file.or_else(|| retrieve_from_cfg()).expect("Could not resolve a vault file location");
 
-    let ansible = Ansible { vault: args.vault_password_file };
+    let ansible = Ansible { vault: vault_file };
     let parser = parser::Parser { ansible };
     let trimmed = content.strip_prefix("---").map_or(content.as_str(), |stripped| stripped.trim());
 
     parser.parse(trimmed);
+}
+
+
+fn retrieve_from_cfg() -> Option<String> {
+    let Some(cfg) = vec!("ansible.cfg", "ansible/ansible.cfg").into_iter().find_map(|path| fs::read_to_string(path).ok()) else {
+        warn!("Could not find ansible.cfg");
+        return None;
+    };
+    let regex: Regex = Regex::new(r"vault_password_file ?= ?(?<file>.*)").unwrap();
+    let maybe_captures = regex.captures(&cfg);
+    if maybe_captures.is_none() {
+        warn!("Could not find 'vault_password_file' in config");
+        return None;
+    }
+    let maybe_file = maybe_captures.unwrap().name("file").map(|m| m.as_str().to_owned());
+    if maybe_file.is_none() {
+        warn!("No match for 'vault_password_file' value");
+    }
+    maybe_file
 }
