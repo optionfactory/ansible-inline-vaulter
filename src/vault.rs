40c8b718a52bac9cfa25077ab34994c8f0dd3a03
Enrico Falanga <enrico.falanga@optionfactory.net>
2023-11-30 10:14:57 +0100

    parse cfg tests

--- src/vault.rs
+++ src/vault.rs
@@ -14,9 +14,9 @@ pub struct Vault {
 impl Vault {
     pub fn from_config() -> Result<Self> {
         let base_dir = env::current_dir()?;
-        let cfg = Self::retrieve_cfg(&base_dir)?;
-        let vault_file = Self::parse_no_id(&cfg);
-        let vault_ids = Self::parse_ids(&cfg);
+        let cfg = retrieve_cfg(&base_dir)?;
+        let vault_file = parse_no_id(&cfg);
+        let vault_ids = parse_ids(&cfg);
 
         if vault_file.is_none() && vault_ids.is_none() {
             return Err(anyhow!("Could not find any vault file in config"));
@@ -24,7 +24,7 @@ impl Vault {
 
         Ok(Vault {
             no_id: vault_file.unwrap_or(String::from("")),
-            ids: Default::default(),
+            ids: vault_ids.unwrap_or(Default::default()),
         })
     }
 
@@ -46,48 +46,122 @@ impl Vault {
         self.ids.get(id).map(|s| s.as_str())
     }
 
-    fn retrieve_cfg(base_path: &Path) -> Result<String> {
-        vec!("ansible.cfg", "ansible/ansible.cfg")
-            .into_iter()
-            .map(|f| base_path.join(f))
-            .find(|p| Path::exists(p))
-            .map(|p| fs::read_to_string(p).unwrap())
-            .ok_or_else(|| anyhow!("Could not find ansible.cfg"))
+}
+
+fn retrieve_cfg(base_path: &Path) -> Result<String> {
+    vec!("ansible.cfg", "ansible/ansible.cfg")
+        .into_iter()
+        .map(|f| base_path.join(f))
+        .find(|p| Path::exists(p))
+        .map(|p| fs::read_to_string(p).unwrap())
+        .ok_or_else(|| anyhow!("Could not find ansible.cfg"))
+}
+
+fn parse_no_id(cfg: &str) -> Option<String> {
+    let regex: Regex = Regex::new(r"vault_password_file ?= ?(?<file>.*)").unwrap();
+    let maybe_captures = regex.captures(cfg);
+    if maybe_captures.is_none() {
+        info!("Could not find 'vault_password_file' in config");
+        return None;
+    }
+    let maybe_file = maybe_captures.unwrap().name("file").map(|m| m.as_str().to_owned());
+    if maybe_file.is_none() {
+        warn!("No match for 'vault_password_file' value");
+    }
+    maybe_file
+}
+
+fn parse_ids(cfg: &str) -> Option<HashMap<String, String>> {
+    let regex: Regex = Regex::new(r"vault_identity_list ?= ?(?<file_list>.*)").unwrap();
+    let maybe_captures = regex.captures(cfg);
+    if maybe_captures.is_none() {
+        info!("Could not find 'vault_identity_list' in config");
+        return None;
+    }
+    let maybe_list = maybe_captures.unwrap().name("file_list").map(|m| m.as_str());
+    if maybe_list.is_none() {
+        warn!("No match for 'vault_identity_list' value");
     }
 
-    fn parse_no_id(cfg: &str) -> Option<String> {
-        let regex: Regex = Regex::new(r"vault_password_file ?= ?(?<file>.*)").unwrap();
-        let maybe_captures = regex.captures(cfg);
-        if maybe_captures.is_none() {
-            info!("Could not find 'vault_password_file' in config");
-            return None;
-        }
-        let maybe_file = maybe_captures.unwrap().name("file").map(|m| m.as_str().to_owned());
-        if maybe_file.is_none() {
-            warn!("No match for 'vault_password_file' value");
-        }
-        maybe_file
+    maybe_list
+        .map(|s| s.split(",").collect::<Vec<&str>>())
+        .map(|v| v.iter()
+            .map(|&s| {
+                let (label, path) = s.split_once("@").unwrap();
+                (label.to_owned(), path.to_owned())
+            }).collect())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use crate::vault::{parse_ids, parse_no_id};
+
+    #[test]
+    fn test_no_id_parse() {
+        let cfg = r#"
+[ssh_connection]
+pipelining = True
+retries = 2
+
+[defaults]
+vault_password_file = ~/.vault/wasd
+"#;
+        let exp = String::from("~/.vault/wasd");
+
+        let act = parse_no_id(cfg);
+
+        assert_eq!(exp, act.unwrap());
     }
 
-    fn parse_ids(cfg: &str) -> Option<HashMap<String, String>> {
-        let regex: Regex = Regex::new(r"vault_identity_list ?= ?(?<file_list>.*)").unwrap();
-        let maybe_captures = regex.captures(cfg);
-        if maybe_captures.is_none() {
-            info!("Could not find 'vault_identity_list' in config");
-            return None;
-        }
-        let maybe_list = maybe_captures.unwrap().name("file_list").map(|m| m.as_str());
-        if maybe_list.is_none() {
-            warn!("No match for 'vault_identity_list' value");
-        }
+    #[test]
+    fn test_no_id_missing_parse() {
+        let cfg = r#"
+[ssh_connection]
+pipelining = True
+retries = 2
 
-        maybe_list
-            .map(|s| s.split(",").collect::<Vec<&str>>())
-            .map(|v| v.iter()
-                .map(|&s| {
-                    let (label, path) = s.split_once("@").unwrap();
-                    (label.to_owned(), path.to_owned())
-                }).collect())
+[defaults]
+
+"#;
+        let act = parse_no_id(cfg);
+
+        assert!(act.is_none())
+    }
+
+    #[test]
+    fn test_ids_parse() {
+        let cfg = r#"
+[ssh_connection]
+pipelining = True
+retries = 2
+
+[defaults]
+vault_identity_list = asd@~/.vault/asd,qwerty@~/.vault/qwerty
+"#;
+        let exp = HashMap::from([
+            (String::from("asd"), String::from("~/.vault/asd")),
+            (String::from("qwerty"), String::from("~/.vault/qwerty"))
+        ]);
+
+        let act = parse_ids(cfg);
+
+        assert_eq!(exp, act.unwrap());
+    }
+
+    #[test]
+    fn test_ids_missing_parse() {
+        let cfg = r#"
+[ssh_connection]
+pipelining = True
+retries = 2
+
+[defaults]
+"#;
+
+        let act = parse_ids(cfg);
+
+        assert!(act.is_none())
     }
 }
 
