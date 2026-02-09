97492b3fca66e93601d83ee585331f78652097b8
Enrico Falanga <enrico.falanga@optionfactory.net>
2026-02-09 14:54:42 +0100

    :sparkles: terminal ui for file selection

--- src/list_selector.rs
+++ src/list_selector.rs
@@ -1,23 +1,31 @@
-use std::collections::{BTreeMap, HashMap};
-use std::hash::Hash;
-use std::path::{Path, PathBuf};
-use text_io::read;
+use anyhow::Result;
+use crossterm::event::{self, KeyCode};
+use log::{error, warn};
+use ratatui::layout::{Constraint, Layout};
+use ratatui::style::{Color, Modifier, Stylize};
+use ratatui::text::{Line, Span};
+use ratatui::widgets::{List, ListState};
+use ratatui::Frame;
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+use std::process::exit;
 
 pub trait ListSelector {
     fn select_one(&self, files: BTreeMap<String, PathBuf>) -> Option<PathBuf>;
 }
 
-pub struct SimpleListSelector {}
+pub struct TuiListSelector {}
 
-impl SimpleListSelector {
-    pub fn new() -> SimpleListSelector {
-        SimpleListSelector {}
+impl TuiListSelector {
+    pub fn new() -> TuiListSelector {
+        TuiListSelector {}
     }
 }
 
-impl ListSelector for SimpleListSelector {
+impl ListSelector for TuiListSelector {
     fn select_one(&self, files: BTreeMap<String, PathBuf>) -> Option<PathBuf> {
         if files.is_empty() {
+            warn!("Could not find any file");
             return None;
         }
 
@@ -25,29 +33,55 @@ impl ListSelector for SimpleListSelector {
             return Some(files.values().next().unwrap().clone());
         }
 
-        println!("Pick one:");
-        (0..files.len())
-            .map(|i| {
-                format!(
-                    "{} - {}",
-                    i,
-                    files.keys().nth(i).unwrap()
-                )
-            })
-            .for_each(|e| println!("{}", e));
-        loop {
-            let read: String = read!();
-            let i: usize = match read.trim().parse() {
-                Ok(num) => {
-                    if num < files.len() {
-                        num
-                    } else {
-                        continue;
+        Self::tui(&files).unwrap_or_else(|e| {
+            error!("Error in Terminal UI: {}", e);
+            exit(1);
+        })
+    }
+}
+
+impl TuiListSelector {
+    pub fn tui(files: &BTreeMap<String, PathBuf>) -> Result<Option<PathBuf>> {
+        let mut list_state = ListState::default().with_selected(Some(0));
+        let mut selected: Option<usize> = None;
+        ratatui::run(|terminal| loop {
+            terminal.draw(|frame| {
+                Self::render(frame, &mut list_state, files.keys().cloned().collect())
+            })?;
+            if let Some(key) = event::read()?.as_key_press_event() {
+                match key.code {
+                    KeyCode::Char('j') | KeyCode::Down => list_state.select_next(),
+                    KeyCode::Char('k') | KeyCode::Up => list_state.select_previous(),
+                    KeyCode::Enter => {
+                        selected = list_state.selected();
+                        break Ok(selected);
                     }
+                    KeyCode::Char('q') | KeyCode::Esc => break Ok(None),
+                    _ => {}
                 }
-                Err(_) => continue,
-            };
-            return Some(files.values().nth(i).unwrap().clone());
-        }
+            }
+        })
+        .map(|s| match s {
+            Some(i) => Some(files.values().nth(i).unwrap().clone()),
+            None => None,
+        })
+    }
+
+    fn render(frame: &mut Frame, list_state: &mut ListState, items: Vec<String>) {
+        let constraints = [Constraint::Length(1), Constraint::Fill(1)];
+        let layout = Layout::vertical(constraints).spacing(1);
+        let [top, first] = frame.area().layout(&layout);
+
+        let title = Line::from_iter([
+            Span::from("Select file to edit").bold(),
+            Span::from(" (Press 'q' to quit and arrow keys to navigate)"),
+        ]);
+        frame.render_widget(title.centered(), top);
+
+        let list = List::new(items)
+            .style(Color::Gray)
+            .highlight_style(Modifier::REVERSED);
+
+        frame.render_stateful_widget(list, first, list_state);
     }
 }
